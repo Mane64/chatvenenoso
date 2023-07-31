3438a2a00a45d0e779cc134646e82ed3f73caa08
Mane64 <manuelbarbosacato@gmail.com>
2023-07-31 16:35:22 -0600

    cambios en la imagen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:chatvenenoso/Canales.dart';
+import 'package:chatvenenoso/screens/signin_screen.dart';
 
 void main() async {
   await Firebase.initializeApp(
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: ChannelListScreen(),
+      home: SignInScreen(),
     );
   }
 }
--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -34,7 +34,7 @@ class _SignInScreenState extends State<SignInScreen> {
                 20, MediaQuery.of(context).size.height * 0.2, 20, 0),
             child: Column(
               children: <Widget>[
-                logoWidget("assets/images/logo1.png"),
+                logoWidget("assets/LOGOUPPECOLOR.png"),
                 const SizedBox(
                   height: 30,
                 ),
