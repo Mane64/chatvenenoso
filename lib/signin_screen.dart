dbdf2ec5ebc21f06e9c4ba4be78d3d666c911c47
adrianoblea850 <adrianoblea850@gmail.com>
2023-07-31 15:28:11 -0600

    no llevo nada xd

---
+++ lib/signin_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SigInScreen extends StatefulWidget {
+  const SigInScreen({super.key});
+
+  @override
+  State<SigInScreen> createState() => _SigInScreenState();
+}
+
+class _SigInScreenState extends State<SigInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+        decoration:BoxDecoration(color: Colors.black),
+      ),
+    );
+  }
+}
